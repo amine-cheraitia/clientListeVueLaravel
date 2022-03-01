a96ec4fd47752648b1fd3c54d519614cf34dd236
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-03-01 10:55:19 +0100

    add any router to web.php

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::view('{any}', 'dashboard')->middleware('auth')->where('any', '.*');
